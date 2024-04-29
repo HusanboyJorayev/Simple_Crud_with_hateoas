a5ac8f2e91a47a29697cb627a28ab9d333a3da44
HusanboyJorayev <144492116+HusanboyJorayev@users.noreply.github.com>
2024-04-29 14:16:37 +0500

    Initial commit

---
+++ src/main/java/com/example/simple_crud_with_hateoas/SimpleCrudWithHateoasApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple_crud_with_hateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrudWithHateoasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCrudWithHateoasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simple_crud_with_hateoas/SimpleCrudWithHateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_crud_with_hateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudWithHateoasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
